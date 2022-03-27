1f8c9fbef16692919c2f4e56cfb9bdb8cd0c0010
Jen Truong <jennifertruong09@gmail.com>
2022-03-27 19:56:08 -0400

    added to schemas and changed server.js to include the schemas and server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,6 +24,47 @@ const resolvers = {
             return { token, user };
         },
 
-        
+        login: async (parent, { email, password } ) => {
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                throw new AuthenticationError('Incorrect email entered.');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if(!correctPassword) {
+                throw new AuthenticationError('Incorrect password entered.');
+            }
+
+            const token = signToken(user);
+            return {token, user};
+        },
+
+        saveBook: async ( parent, {input}, context ) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Please login first.')
+        },
+
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: {savedBooks: { bookId: args.bookId}}},
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Please login first.')
+        }
     },
 }
+
+module.exports = resolvers
