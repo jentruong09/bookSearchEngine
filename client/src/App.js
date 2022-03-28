da48f6e5b4346a8944904b10f5b8e6d09666c211
Jen Truong <jennifertruong09@gmail.com>
2022-03-28 09:22:31 -0400

    added the routes using apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+})
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
-        <Routes>
+        <Switch>
+          <Route exact path = '/' component={SearchBooks}/>
+          <Route exact path = '/saved' component={SavedBooks}/>
+          <Route render = {() => <h1 className='message'> Wrong Page/Route! </h1>} />
+        </Switch>
+        {/* <Routes>
           <Route 
             path='/' 
             element={<SearchBooks />} 
@@ -22,9 +44,10 @@ function App() {
             path='*'
             element={<h1 className='display-2'>Wrong page!</h1>}
           />
-        </Routes>
+        </Routes> */}
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
